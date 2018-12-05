d1f3779e4f4512f092539db442539ebe2734e28d
jacekku <jacekwyroba97@gmail.com>
2018-12-05 16:34:16 +0100

    handleJSON

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $.getJSON('data.json', function (response) {
-        console.log(response);
+        handleJSON(response);
     });
 });
+
+
+
+
+
+function handleJSON(json){
+    json.forEach(element => {
+        document.write("<h1>"+element+"</h1>")
+    });
+}
