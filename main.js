eb2e0f32dd4e4214b0eee64d09aa3ab7c55d5452
jacekku <jacekwyroba97@gmail.com>
2018-12-05 16:41:57 +0100

    added json handling

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ $(document).ready(function () {
 
 
 function handleJSON(json){
-    json.forEach(element => {
-        document.write("<h1>"+element+"</h1>")
-    });
+    document.write(`<H1>Liczba osób na Inżynierii oprogramowania <br/>${json.InzOP}</H1>`)
+    document.write(`<H1>Liczba osób na Systemach informacyjnych <br/>${json.SysInf}</H1>`)
+    document.write(`<H3>Last Update ${json.UpdatedAt}</H3>`)
 }
+
+// handleJSON({"InzOP": 44, "SysInf": 7, "UpdatedAt": "Wed Dec  5 16:17:42 2018"})
