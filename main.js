a1419f039061a36e310cbcf66bb0273ad4d4af15
jacekku <jacekwyroba97@gmail.com>
2018-12-05 18:51:07 +0100

    fixed js maybe

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-$(document).ready(function () {
-    $.getJSON('data.json', function (response) {
-        handleJSON(response);
-    });
-});
+// $(document).ready(function () {
+//     $.getJSON('data.json', function (response) {
+//         handleJSON(response);
+//     });
+// });
 
 
-
+function getElement(string,type="H1"){
+let elem = document.createElement(type);
+elem.innerHTML=string
+// Wstawia go na koniec ciała dokumentu
+return elem
+}
 
 
 function handleJSON(json){
-    document.write(`<H1>Liczba osób na Inżynierii oprogramowania <br/>${json.InzOP}</H1>`)
-    document.write(`<H1>Liczba osób na Systemach informacyjnych <br/>${json.SysInf}</H1>`)
-    document.write(`<H3>Last Update ${json.UpdatedAt}</H3>`)
+    document.body.appendChild(getElement(`Liczba osób na Inżynierii oprogramowania <br/>${json.InzOP}`))
+    document.body.appendChild(getElement(`Liczba osób na Systemach informacyjnych <br/>${json.SysInf}`))
+    document.body.appendChild(getElement(`Last Update ${json.UpdatedAt}`,"H3"))
 }
 
-// handleJSON({"InzOP": 44, "SysInf": 7, "UpdatedAt": "Wed Dec  5 16:17:42 2018"})
+handleJSON({"InzOP": 44, "SysInf": 7, "UpdatedAt": "Wed Dec  5 16:17:42 2018"})
